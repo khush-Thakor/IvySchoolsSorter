0c36f9062f12bca410282aa0a8b4af91e8b89fdb
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-08-19 22:09:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.getElementById("backButtonArea").style.display = 'none';
+for (let i = 1; i < 9; i++) {
+document.getElementById("button"+i.toString()).addEventListener("click",function(){
+  nextButton();
+});
+}
+document.getElementById('backButton').addEventListener("click",function(){
+  backButton();
+});
+
+function nextButton(){
+  document.getElementById('welcomePage').style.display = 'none';
+  document.getElementById("backButtonArea").style.display = 'block';
+}
+function backButton(){
+  document.getElementById('welcomePage').style.display = 'block';
+  document.getElementById("backButtonArea").style.display = 'none';
+}
 
