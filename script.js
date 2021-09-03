e672bb5d425677e115a6857ec83edfd9ea01aa4a
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-09-02 22:24:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var selection = "";
+
+window.onload = function(){
+let button = document.getElementById("dropDownButton");
+let e = document.getElementById("acceptanceRateButton");
+let k = document.getElementById("studentSizeButton");
+let c = document.getElementById("staffSizeButton");
+button.innerHTMl = "Khush";
+e.addEventListener("click",function(){
+  selection = "acceptanceRate";
+  button.innerHTMl = "Khush";
+});
+k.addEventListener("click",function(){
+  selection = "studentSize";
+});
+c.addEventListener("click",function(){
+  selection = "staffSize";
+});
+}
 
