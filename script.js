592bd3a1165e32a04624b034459f1c41522e8506
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-09-03 22:32:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+
 var selection = "";
+var acceptanceRates  = [];
 
 window.onload = function(){
 let button = document.getElementById("dropDownButton");
 let e = document.getElementById("acceptanceRateButton");
 let k = document.getElementById("studentSizeButton");
 let c = document.getElementById("staffSizeButton");
+
+
 button.innerHTMl = "Khush";
 e.addEventListener("click",function(){
   selection = "acceptanceRate";
-  button.innerHTMl = "Khush";
+  document.getElementById("dropDownButton").innerHTML = "Acceptance Rate";
 });
 k.addEventListener("click",function(){
   selection = "studentSize";
+  document.getElementById("dropDownButton").innerHTML = "Student Size";
 });
 c.addEventListener("click",function(){
   selection = "staffSize";
+  document.getElementById("dropDownButton").innerHTML = "Staff Size";
 });
 }
-
