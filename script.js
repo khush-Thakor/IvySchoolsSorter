22463cf57c3034012f34c341d1501c452503bb09
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-09-04 22:26:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 var selection = "";
+var collegeNames = ["Brown","Columbia","Cornell","Dartmouth","Harvard","Princeton","University of Pennsylvania","Yale"]
+var collegeImages = ["brownImage.png","columbiaImage.png","cornellImage.png","dartmouthImage.png","harvardImage.jpg","princetonImage.png","uPennImage.png","yaleImage.png"]
 var acceptanceRates  = [];
 
 window.onload = function(){
@@ -7,6 +9,11 @@ let button = document.getElementById("dropDownButton");
 let e = document.getElementById("acceptanceRateButton");
 let k = document.getElementById("studentSizeButton");
 let c = document.getElementById("staffSizeButton");
+for (let i = 1;i<9;i++){
+    console.log("collegeName"+i.toString());
+  document.getElementById("collegeName"+i).innerHTML = collegeNames[i-1];
+  document.getElementById("collegeImage"+i).src = collegeImages[i-1];
+}
 
 
 button.innerHTMl = "Khush";
