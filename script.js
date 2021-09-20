38d036ba91904fa4ccc728e5ce03abfce9ae9021
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-09-19 22:51:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
 
-var selection = "";
-var collegeNames = ["Brown","Columbia","Cornell","Dartmouth","Harvard","Princeton","University of Pennsylvania","Yale"]
-var collegeImages = ["collegeImages/brownImage.png","collegeImages/columbiaImage.png","collegeImages/cornellImage.png","collegeImages/dartmouthImage.png","collegeImages/harvardImage.jpg","collegeImages/princetonImage.png","collegeImages/uPennImage.png","collegeImages/yaleImage.png"]
-var acceptanceRatesList =[5.4,3.7,8.7,6.17,3.43,5.63,5.68,4.62];
-const acceptanceRates = new Map();
-const collegeImagesMap = new Map();
-collegeImagesMap.set("Brown","collegeImages/brownImage.png");
-collegeImagesMap.set("Columbia","collegeImages/columbiaImage.png");
-collegeImagesMap.set("Cornell","collegeImages/cornellImage.png");
-collegeImagesMap.set("Dartmouth","collegeImages/dartmouthImage.png");
-collegeImagesMap.set("Harvard","","collegeImages/harvardImage.jpg");
-collegeImagesMap.set("Princeton","collegeImages/princetonImage.png");
-collegeImagesMap.set("University of Pennsylvania","collegeImages/yaleImage.png");
-collegeImagesMap.set("Yale","yaleImage.png");
 
-acceptanceRates.set(5.4,"Brown");
-acceptanceRates.set(3.7,"Columbia");
-acceptanceRates.set(8.7,"Cornell");
-acceptanceRates.set(6.17,"Dartmouth");
-acceptanceRates.set(3.43,"Harvard");
-acceptanceRates.set(5.63,"Princeton");
-acceptanceRates.set(5.68,"University of Pennsylvania");
-acceptanceRates.set(4.62,"Yale");
+var collegeNames = ["Brown","Columbia","Cornell","Dartmouth","Harvard","Princeton","University of Pennsylvania","Yale"]
+
+var collegeImages = ["collegeImages/brownImage.png","collegeImages/columbiaImage.png","collegeImages/cornellImage.png","collegeImages/dartmouthImage.png","collegeImages/harvardImage.jpg","collegeImages/princetonImage.png","collegeImages/uPennImage.png","collegeImages/yaleImage.png"];
+var acceptanceRatesList =[5.4,3.7,8.7,6.17,3.43,5.63,5.68,4.62];
+var studentSizesList = [9948,6000,23620,4170,23731,8856,21785,14810];
+var collegeRatiosList = [6,6.1,9,7,5,5.1,6.2,4];
+const acceptanceRates = new Map();
+const studentSizes = new Map();
+const collegeImagesMap = new Map();
+const collegeRatiosMap = new Map();
+
+for (let i = 0;i<8;i++){
+  collegeImagesMap.set(collegeNames[i],collegeImages[i]);
+  acceptanceRates.set(acceptanceRatesList[i],collegeNames[i]);
+  studentSizes.set(studentSizesList[i],collegeNames[i]);
+  collegeRatiosMap.set(collegeRatiosList[i],collegeNames[i]);
+}
+
 
 window.onload = function(){
 let button = document.getElementById("dropDownButton");
 let e = document.getElementById("acceptanceRateButton");
 let k = document.getElementById("studentSizeButton");
-let c = document.getElementById("staffSizeButton");
+let c = document.getElementById("studentRatioButton");
+document.getElementById('collegeInformation').style.display = "none";
 for (let i = 1;i<9;i++){
-  document.getElementById("collegeName"+i).innerHTML = collegeNames[i-1];
+  document.getElementById("collegeName"+i).innerHTML = i+". "+collegeNames[i-1];
   document.getElementById("collegeImage"+i).src = collegeImages[i-1];
 }
 
-
-button.innerHTMl = "Khush";
 e.addEventListener("click",function(){
-  selection = "acceptanceRate";
   document.getElementById("dropDownButton").innerHTML = "Acceptance Rate";
   sortAcceptanceRates();
 });
 k.addEventListener("click",function(){
-  selection = "studentSize";
   document.getElementById("dropDownButton").innerHTML = "Student Size";
+  sortStudentSizes();
 });
 c.addEventListener("click",function(){
-  selection = "staffSize";
   document.getElementById("dropDownButton").innerHTML = "Staff Size";
+  sortStudentRatios();
 });
+
+for (let i = 0;i<8;i++){
+  document.getElementById("collegeButton"+(i+1)).addEventListener("click",function (){
+    document.getElementById('collegeInformation').style.display = "block";
+    document.getElementById("collegeImage").src = collegeImages[i];
+    document.getElementById("collegeAcceptanceRateText").innerHTML = acceptanceRatesList[i];
+    document.getElementById("collegeStudentSizeText").innerHTML = studentSizesList[i];
+    if (collegeRatiosList[i]==6.1||collegeRatiosList[i]==6.2)
+    document.getElementById("collegeRatioText").innerHTML = "6:1";
+    else if(collegeRatiosList[i]==5.1)
+    document.getElementById("collegeRatioText").innerHTML = "5:1";
+    else
+    document.getElementById("collegeRatioText").innerHTML = collegeRatiosList[i]+":1";
+  });
+}
+
 }
 
 function sortAcceptanceRates(){
@@ -66,9 +76,45 @@ function sortAcceptanceRates(){
   }
   updateScreen();
 }
+
+function sortStudentSizes(){
+studentSizesList.sort(function(a,b){
+  return b-a
+});
+let namesSorted = [" "," "," "," "," "," "," "," "];
+for (let i = 0;i<8;i++){
+  namesSorted[i] = studentSizes.get(studentSizesList[i]);
+}
+for (let i = 0;i<8;i++){
+  collegeNames[i] = namesSorted[i];
+}
+for (let i = 0;i<8;i++){
+  collegeImages[i] = collegeImagesMap.get(namesSorted[i]);
+}
+updateScreen();
+}
+
+function sortStudentRatios(){
+  collegeRatiosList.sort(function(a,b){
+    return b-a;
+  });
+  let namesSorted = [" "," "," "," "," "," "," "," "];
+  for(let i = 0;i<8;i++){
+    namesSorted[i] = collegeRatiosMap.get(collegeRatiosList[i]);
+  }
+  for (let i = 0;i<8;i++){
+    collegeNames[i] = namesSorted[i];
+  }
+  for (let i = 0;i<8;i++){
+    collegeImages[i] = collegeImagesMap.get(namesSorted[i]);
+  }
+  updateScreen();
+}
+
+
 function updateScreen(){
   for (let i = 1;i<9;i++){
     document.getElementById("collegeImage"+i).src = collegeImages[i-1];
-    document.getElementById("collegeName"+i).innerHTML = collegeNames[i-1];
+    document.getElementById("collegeName"+i).innerHTML = i+". "+collegeNames[i-1];
   }
 }
